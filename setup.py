4ab64cfb2aec915b786c48801a9e6e7dea60fad1
Han Y.B <yanbohan98@gmail.com>
2021-05-28 03:26:55 +0800

    Dev fullerene algorithm (#8)
    * Establish the initiate version for finding cycles with boost::graph using cython.
    * package fullerenedatapraser.graph.algorithm now have module dual with class py_graph_circle_finder for a cycle finder instance, in which function get_face_vertex_list will return a list of numpy.array consist of all circle vertex index.
    * Warning: no error except for non-planarity graph and check. It will be an undefined behavior.

--- setup.py
+++ setup.py
@@ -6,11 +6,28 @@
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
+
+from Cython.Build import cythonize
+from setuptools import Extension
 from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        import numpy
+
+        extensions = [  # *find_pyx()
+            Extension("fullerenedatapraser.graph.algorithm.dual", ["src/fullerenedatapraser/graph/algorithm/dual.pyx"],
+                      include_dirs=[numpy.get_include(), "e3rdpackage/boost"],
+                      language="c++",
+                      # libraries=[],
+                      # library_dirs=[],
+                      ),
+        ]
+        setup(use_scm_version={"version_scheme": "no-guess-dev"},
+              ext_modules=cythonize(extensions, language_level=3),
+              # include_dirs=[numpy.get_include()],
+              # packages=find_packages()
+              )
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
