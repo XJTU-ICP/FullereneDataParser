dd0634740a713608b7fd39447efef38b1ec5959f
saltball <yanbohan98@gmail.com>
2021-06-09 03:03:48 +0800

    - Bug Fixed: Algorithm of search addon vertex positions of fullerene cage.

--- src/fullerenedatapraser/graph/algorithm/cagesym.py
+++ src/fullerenedatapraser/graph/algorithm/cagesym.py
@@ -7,8 +7,10 @@
 # ====================================== #
 
 import numpy as np
+import tqdm
 
-def search_addon(ADJ:np.ndarray,n:int,condition_for_vertex=None,mask=None,searched=None):
+
+def _search_addon(ADJ:np.ndarray,n:int,mask=None,searched=None):
     """
 
     Parameters
@@ -17,67 +19,72 @@ def search_addon(ADJ:np.ndarray,n:int,condition_for_vertex=None,mask=None,search
         Adjecant Matrix of Cage Graph.
     n:int
         Addons to do.
-    condition_for_vertex:
-        not used
     Returns
     -------
-    List of vertex for addons.
+    List of vertex for addons. **With repeated!!!**
     """
     assert len(ADJ.shape)==2
     assert ADJ.shape[0]==ADJ.shape[1]
     assert ADJ.shape[0]%2==0
     natoms=ADJ.shape[0]
+    if searched is None:
+        searched = []
+    searched_main=searched.copy()
     assert isinstance(n,int), "`n` must be an integer and n>=1 !"
     if mask is None:
         mask=[]
-    if searched is None:
-        searched=[]
-    else:
-        searched=searched
     if n>1:
         for item in get_vertex_class_lists(ADJ,mask):
             # iter for each class of vertex
             ADJ_use = ADJ.copy()
+            searched_sub = searched_main.copy()
 
             addonlist=[]
             addoni=min(item)
             ADJ_use[addoni,:]=0
             ADJ_use[:,addoni]=0
             addonlist.append(addoni)
-            for subaddons in search_addon(ADJ_use,n-1,mask=[*mask,addoni],searched=searched):
+            for subaddons in search_addon(ADJ_use,n-1,mask=[*mask,addoni],searched=searched_sub):
                 yield [addoni,*subaddons]
-            searched.extend(item)
+            # searched_main.extend(item)
 
     elif n==1:
         for item in get_vertex_class_lists(ADJ,mask):
             result=min(item)
-            if result not in searched:
+            if result not in searched_main:
                 yield [min(item)]
             else:
                 pass
     else:
         raise RuntimeError("Unkown error in searching addons of cages, got n<=0!")
 
+def search_addon(ADJ:np.ndarray,n:int,mask=None,searched=None):
+    """
+    See Also `_search_addon()`
+
+    """
+    return np.unique(np.sort(np.array(list(_search_addon(ADJ,n,mask,searched))), axis=-1), axis=0)
 
 
-
-def get_vertex_class_lists(ADJ,mask:list):
+def get_vertex_class_lists(ADJ,mask:list,order=None):
     natoms=ADJ.shape[0]
     nocc=int(natoms/2)
-    p = np.zeros([natoms, natoms])
-    val,vec = np.linalg.eigh(ADJ)
-
-    for i in range(natoms):
-        for j in range(natoms):
-            for l in range(nocc):
-                p[i][j] += 2 * vec[i][l] * vec[j][l]
-
-    cc = np.array([np.sort(np.array(list(i))) for i in np.round(p * ADJ, 8)])
-    cc_norm = np.linalg.norm(cc, axis=-1)
-    cc_mat = (cc[None, :, :] * cc[:, None, :]).sum(-1) / ((cc_norm[None, :] * cc_norm[:, None])+0.001)
-
-    vertex_class = np.unique(cc_mat, axis=-1, return_index=True, return_inverse=True)
+    natoms = ADJ.shape[0]
+    nocc = int(natoms / 2)
+    val, vec = np.linalg.eigh(ADJ)
+    if not order:
+        order = natoms//4*2
+    else:
+        pass
+    result = vec @ np.diag(val ** order) @ np.linalg.inv(vec)
+    # Use graph transmittion multiple methods for graph symmetry.
+    cc = np.array([np.sort(np.array(list(i))) for i in result])
+    cc_mat = cc #(cc[None, :, :] * cc[:, None, :]).sum(-1)
+    val_max=cc_mat.max()
+    vertex_class = np.unique(np.round(cc_mat/val_max,6), axis=0, return_index=True, return_inverse=True)
+    # Uniform the value to regulate range for unique() function. Otherwise the double values will show differences.
     vertex_class_t = list(np.where(vertex_class[-1] == vertex_class[-1][item])[0] for item in vertex_class[-2])
+
     vertex_class_flitered=[]
     for item in vertex_class_t:
         flag=0
@@ -95,24 +102,29 @@ if __name__ == '__main__':
     from fullerenedatapraser.io.xyz import simple_read_xyz_xtb
     from fullerenedatapraser.molecular.fullerene import FullereneFamily
     from ase.visualize import view
+    import os
+
 
     np.set_printoptions(threshold=np.inf)
     np.set_printoptions(linewidth=500)
 
-    atoms = list(simple_read_xyz_xtb(r"C:\Work\CODE\DATA\fullerxTBcal\xTBcal\opted\C60\C60_000001812opted.xyz"))[-1]
+    atoms = list(simple_read_xyz_xtb(r"C:\Work\CODE\DATA\fullerxTBcal\xTBcal\opted\C44\C44_000000001opted.xyz"))[-1]
+    outputfile=r"C60_1.txt"
+    output=False
 
     # view(atoms)
 
-    f = FullereneFamily(spiral=1812, atoms=atoms)
+    f = FullereneFamily(spiral=1, atoms=atoms)
     f = f.get_fullerenecage()
     D = f.atomADJ
-    # D[1,:]=0
-    # D[:,1]=0
-    # for i in range(3,60):
-    #     D[i,:]=0
-    #     D[:,i]=0
-    # print(D)
-    # [print(item) for item in get_vertex_class_lists(D,mask=[])]
-    addonslist=search_addon(D,4)
-    for position in addonslist:
-        print(position)
+    #
+    print(get_vertex_class_lists(D,[]))
+    # if os.path.exists(outputfile) and output:
+    #     os.remove(outputfile)
+    #
+    # addonslist=search_addon(D,4)
+    #
+    # # print(len(addonslist))
+    # # print(np.unique(np.sort(np.array(addonslist), axis=-1), axis=0))
+    # # print(len(np.unique(np.sort(np.array(addonslist), axis=-1), axis=0)))
+    # print(addonslist,file=open(outputfile,"a"))
