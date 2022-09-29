010778c3ee0a2424dbd089a82da8b3c1633ebf2b
HanYanbo <yanbohan98@gmail.com>
2022-09-29 13:40:43 +0800

    feat(xcsi): add poav orbital calculation.

---
+++ examples/xcsi/orbital_csi_poav.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+# ====================================== #
+# @Author  : Yanbo Han
+# @Email   : yanbohan98@gmail.com
+# @File    : origin_csi.py
+# ALL RIGHTS ARE RESERVED UNLESS STATED.
+# ====================================== #
+
+__doc__ = """
+# ================ NOTE ================ #
+# Request packages: `numpy`,`matplotlib`
+# 1. Setup data source.
+# 2. Calculate if needed.
+# 3. Draw images of data.
+# ================ NOTE ================ #
+"""
+
+import pathlib
+from functools import partial
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from fullerenedataparser.calculator.extend_csi import mp_store_csi, calculate_ext_csi
+from fullerenedataparser.molecular.fullerene import FullereneFamily
+from fullerenedataparser.util.geometry import poav_of_four_points
+from utils import charge_name_parse, charges_draw_parse, calculate_xcsi
+
+plt.rcParams['font.sans-serif'] = "Arial"
+
+def calculate_ext_csi(fullerene: FullereneFamily, para=7, distance_cutoff=None):
+    """
+    Implemention of extended
+    Parameters
+    ----------
+    fullerene:FullereneFamily
+        molecule Instance of Fullerene.
+    para:int
+        multiple value to adj
+    distance_cutoff: float or None
+        distance more than cutoff will be regard as 0
+    Returns
+    -------
+        CSI
+    References
+    ----------
+
+    """
+    assert fullerene.natoms % 2 == 0, f"Not A classical Fullerene. Check your input atoms: {fullerene}."
+    assert fullerene.info[
+               "charge"] % 2 == 0, f"Only Deal with Even number charged Fullerene Now. Got charge:{fullerene.info['charge']}."
+    csi_adj = fullerene.atomADJ  # not used actually
+
+    distances = fullerene.get_all_distances()
+    mask = np.ones_like(distances) - np.eye(fullerene.natoms)
+    # dis_vec = fullerene.get_all_distances(vector=True)
+    # dis_vec /= (distances + 0.00001)[:, :, None]
+    # orbital_p_vec = (distances[:, :, None] * dis_vec).sum(-2)
+    # orbital_p_vec /= -np.linalg.norm(orbital_p_vec, 2, 1)[:, None]
+
+    orbital_p_vec = np.zeros([fullerene.natoms, 3])
+    for i in range(fullerene.natoms):
+        vec = poav_of_four_points(
+            fullerene.positions[np.where(fullerene.atomADJ[i] == 1)][0],
+            fullerene.positions[np.where(fullerene.atomADJ[i] == 1)][1],
+            fullerene.positions[np.where(fullerene.atomADJ[i] == 1)][2],
+            fullerene.positions[i])
+        orbital_p_vec[i] = vec
+    orbital_p_vec /= -np.linalg.norm(orbital_p_vec, 2, 1)[:, None]
+    if distance_cutoff:
+        raise NotImplementedError
+    orbital_p_cos = (orbital_p_vec[None, :] * orbital_p_vec[:, None]).sum(-1) / 2 + 0.5
+    mask = csi_adj * mask
+
+    extend_adj = orbital_p_cos*np.exp(-distances**2)
+
+    sum_chi = np.linalg.eigh(extend_adj * mask)
+
+    adj = fullerene.get_fullerenecage().circleADJ
+    Napp = (adj * (adj.sum(-1) == 5)[None, :] * (adj.sum(-1) == 5)[:, None]).sum() / 2
+    # sum_chi = sum_chi[:fullerene.natoms // 2 - int(fullerene.info["charge"])//2]
+
+    return sum_chi[0], sum_chi[1], Napp
+
+if __name__ == '__main__':
+    _ = \
+        """
+    # =============== PART 1 ================ #
+    1. Setup data source.
+
+    SPIRAL_ATOMS_ADJ_DIR:
+        Directory storing output files of `spiral` program.
+        As an example, file `C20` is:
+    >   # General fullerene of C20:
+    >       <spiral ID> <PG> <Pentagon index> <NMR>
+    >       <ADJ matrix, shape [20,20]>
+    >       ...
+
+    SPIRAL_CIRCLE_ADJ_DIR:
+        Directory storing output files of `spiral` program.
+        As an example, file `C20` is:
+    >   # General fullerene of C20:
+    >       <spiral ID> <PG> <Pentagon index> <NMR>
+    >       <ADJ matrix of circles, shape [12,12]>
+    >       ...
+
+    XYZ_STORE_PREFIX:
+        Prefix name of Directory storing files of C<2N> coordinates particularly.
+        Directory structure should be:
+            XYZ_STORE_PREFIX_<...>
+            |-C20
+            | |-C20_000000001opt.xyz
+            | ...
+            |-C24
+            |...
+
+        These .xyz files have energy calculated by `xTB`.
+        As an example, file `C20_000000001opt.xyz` is:
+    >   20
+    >    energy: -42.0890 <info>
+    >   C  <xyz coordinates>
+    >   ...
+
+    CSI_NPY_STORE_PREFIX:
+        Prefix name of Directory storing files of C<2N>  particularly.
+        See `numpy.load()` and `numpy.savez()` for details.
+        Each `.npz` file have three `numpy.array()` named:
+        ['csi_list', 'spiral_num', 'energy', 'napp']
+        CSI_NPY_STORE_PREFIX<...>
+            |-C20_CSI.npz
+            |-C24_CSI.npz
+            |...
+
+    # =============== PART 1 ================ #
+    """
+
+    SPIRAL_ATOMS_ADJ_DIR = r"D:\CODE\#DATASETS\FullDB\atomadj"  # change this
+    SPIRAL_CIRCLE_ADJ_DIR = r"D:\CODE\#DATASETS\FullDB\circleadj"  # change this
+    XYZ_STORE_PREFIX = r"D:\CODE\#DATASETS\FullDB\xTBcal"  # change this
+    CSI_NPY_STORE_PREFIX = r"D:\CODE\#DATASETS\FullDB\c2addonwork\xCSI_POAV"  # change this
+    if not pathlib.Path(XYZ_STORE_PREFIX).exists():
+        pathlib.Path(XYZ_STORE_PREFIX).mkdir(parents=True)
+    if not pathlib.Path(CSI_NPY_STORE_PREFIX).exists():
+        pathlib.Path(CSI_NPY_STORE_PREFIX).mkdir(parents=True)
+
+    charged_list = [
+        0,
+        -2,
+        -4,
+        -6,
+        2,
+        4,
+        6
+    ]
+
+    CSI_NPY_FILE_SUFFIX = "xCSI_POAV"
+    _ = \
+        """
+    # =============== PART 2 ================ #
+    2. Calculate if needed.
+    Set `_recalculate` = `True` if force to recalculate all data.
+
+    # =============== PART 2 ================ #
+    """
+    _recalculate = True  # set to True to recalculate all
+
+    _pass = False  # set to True to force pass calculation
+
+    include_traj = False  # set to `False` to only calculate the optimized isomers (most stable configurations)
+
+    number_mask = range(30, 72, 2)
+    # Set to None if not need mask
+    print("Calculating...")
+    if not _pass:
+        for charge in charged_list:
+            charge_in_name = charge_name_parse(charge)
+            if _recalculate:
+                print("Recalculate all without any delete, be careful.")
+                mp_store_csi(atomdir=SPIRAL_ATOMS_ADJ_DIR,
+                             circledir=SPIRAL_CIRCLE_ADJ_DIR,
+                             xyz_root_dir=XYZ_STORE_PREFIX + charge_in_name,
+                             target_dir=CSI_NPY_STORE_PREFIX + charge_in_name,
+                             number_mask=number_mask,
+                             npz_file_suffix=CSI_NPY_FILE_SUFFIX,
+                             _func=calculate_ext_csi,
+                             include_traj=include_traj)
+            else:
+                mp_store_csi(atomdir=SPIRAL_ATOMS_ADJ_DIR,
+                             circledir=SPIRAL_CIRCLE_ADJ_DIR,
+                             xyz_root_dir=XYZ_STORE_PREFIX + charge_in_name,
+                             target_dir=CSI_NPY_STORE_PREFIX + charge_in_name,
+                             npz_file_suffix=CSI_NPY_FILE_SUFFIX,
+                             number_mask=number_mask,
+                             recalculate=False,
+                             _func=calculate_ext_csi,
+                             include_traj=include_traj)
+    print("Calculated.")
+    input()
+    _ = \
+        """
+    # =============== PART 3 ================ #
+    3. Calculate if needed.
+
+
+    # =============== PART 2 ================ #
+    """
+    draw_list = range(60, 62, 2)
+    draw_rc = np.array(list((map(charges_draw_parse, charged_list))))
+
+    napp_value_all = np.array([])
+    xcsilist_only_xcsi_all = np.array([])
+    enlist_all = np.array([])
+    charge_all = np.array([])
+    print("Drawing...")
+    for N in draw_list:
+        # fig, ax = plt.subplots(nrows=3, ncols=max(draw_rc[:, 1]) + 1)
+        for charge in charged_list:
+            charge_in_name = charge_name_parse(charge)
+            pltidx = charges_draw_parse(charge_number=charge)
+            file = (pathlib.Path(CSI_NPY_STORE_PREFIX + charge_in_name) / f"C{N}_{CSI_NPY_FILE_SUFFIX}.npz").as_posix()
+            file = np.load(file)
+            csi_value = file["csi_list"]
+            energy_value = file["energy"]
+            min_en = min(energy_value)
+            napp_value = file['napp']
+            # set `napp_value` to None to use xcsi without napp values.
+            xcsilist = calculate_xcsi(csi_value, napp_value, charge)
+            xcsilist_only_xcsi = calculate_xcsi(csi_value, None, charge)
+            enlist = [item for item in energy_value]
+            # ax[pltidx[0], pltidx[1]].scatter(xcsilist, enlist, marker="x")
+            # ax[pltidx[0], pltidx[1]].set_xlabel("XCSI of {} isomers, cutoff={}".format(f"C$_{{{N}}}^{{{str(charge) + '+' if charge > 0 else '' if charge >= 0 else str(abs(charge)) + '-'}}}$", DIS_CUT))
+            # ax[pltidx[0], pltidx[1]].set_ylabel("Relative energy of {} isomers".format(f"C$_{{{N}}}^{{{str(charge) + '+' if charge > 0 else '' if charge >= 0 else str(abs(charge)) + '-'}}}$", DIS_CUT))
+            # print(charge, np.corrcoef(np.array([xcsilist, napp_value, xcsilist_only_xcsi, enlist])))
+            napp_value_all = np.hstack([napp_value_all, napp_value])
+            xcsilist_only_xcsi_all = np.hstack([xcsilist_only_xcsi_all, xcsilist_only_xcsi])
+            enlist_all = np.hstack([enlist_all, enlist])
+            charge_all = np.hstack([charge_all, np.ones_like(enlist) * charge])
+
+        data = pd.DataFrame(data={
+            "napp_value": napp_value_all, "xcsilist_only_xcsi": xcsilist_only_xcsi_all, "enlist": enlist_all, "charge": charge_all
+        })
+        g = sns.scatterplot(data=data, x="xcsilist_only_xcsi", y="enlist", hue="charge")
+        g.set_xlabel("XCSI")
+        g.set_ylabel("$E_\\mathrm{r}$/eV")
+plt.show()
--- src/fullerenedataparser/util/geometry.py
+++ src/fullerenedataparser/util/geometry.py
@@ -54,3 +54,107 @@ def sphere_center_of_four_points(point_a: np.ndarray, point_b: np.ndarray,
         (dis_matrix ** 2).sum(axis=-1)) / 2
     O = np.linalg.inv(center_matrix[:3]) @ B[:3]
     return O
+
+
+def poav_of_four_points(point_a: np.ndarray,
+                        point_b: np.ndarray,
+                        point_c: np.ndarray,
+                        point_pi: np.ndarray) -> np.ndarray:
+    pos_list = np.array([point_a, point_b, point_c, point_pi])
+    pos_tensor = pos_list[:3] - pos_list[-1]
+    unit_matrix = pos_tensor / np.linalg.norm(pos_tensor, axis=-1)[:, None]
+    cos_matrix = (unit_matrix[None, :] * unit_matrix[:, None]).sum(
+        -1) / 2 + 0.5
+    vec1 = pos_tensor[2] * cos_matrix[0][1] - pos_tensor[1] * cos_matrix[2][0]
+    vec2 = pos_tensor[0] * cos_matrix[1][2] - pos_tensor[2] * cos_matrix[0][1]
+    pi_vec = np.cross(vec2, vec1)
+    # make sure pi_vec point to outside
+    if np.inner(pos_tensor[:3].sum(0), pi_vec) > 0:
+        pi_vec = -pi_vec
+    return pi_vec / np.linalg.norm(pi_vec)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib.widgets import Slider, Button
+
+    a = np.array([3.0908709, -1.1585005, 1.201424])
+    e = np.array([3.1879245, -1.4574599, -0.1997005])
+    c = np.array([3.2984981, -0.4301142, -1.1204138])
+    b = np.array([2.3360057, -2.5814499, -0.476105])
+    bond = np.linalg.norm(a - e)
+    vecAE = a - e
+    vecBE = b - e
+    vecCE = c - e
+    vecAE /= np.linalg.norm(vecAE)
+    vecBE /= np.linalg.norm(vecBE)
+    vecCE /= np.linalg.norm(vecCE)
+    sqrt3 = np.sqrt(3) / 3
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ABC, = ax.plot(*np.array([a, b, c]).transpose(), marker="o")
+    D = ax.scatter(*e, c='b')
+    POAV, = ax.plot(*(poav_of_four_points(a, b, c, e) + e), marker="o")
+    sphere_c = sphere_center_of_four_points(a, b, c, e)
+    sphere_vec = e - sphere_c
+    sphere_vec /= np.linalg.norm(sphere_vec)
+    simple_sphere, = ax.plot(*(sphere_vec + e), marker="o")
+    # ax.axis([0, 1, -10, 10])
+
+    axcolor = 'lightgoldenrodyellow'
+
+    bondA = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+    bondB = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+    bondC = plt.axes([0.25, 0.2, 0.65, 0.03], facecolor=axcolor)
+
+    sbondA = Slider(bondA, 'bondA', 1.3, 2, valinit=bond)
+    sbondB = Slider(bondB, 'bondB', 1.3, 2, valinit=bond)
+    sbondC = Slider(bondC, 'bondC', 1.3, 2, valinit=bond)
+
+
+    def update(val):
+        bA = sbondA.val
+        bB = sbondB.val
+        bC = sbondC.val
+        newA = [vecAE[0] * bA, vecAE[1] * bA, vecAE[2] * bA] + e
+        newB = [vecBE[0] * bB, vecBE[1] * bB, vecBE[2] * bB] + e
+        newC = [vecCE[0] * bC, vecCE[1] * bC, vecCE[2] * bC] + e
+        print(newA, newB, newC)
+        ABC.set_data_3d(*np.array([newA, newB, newC]).transpose())
+        POAV.set_data_3d(*
+                         (poav_of_four_points(np.array(newA),
+                                              np.array(newB),
+                                              np.array(newC),
+                                              e) + e))
+        sphere_c = sphere_center_of_four_points(np.array(newA),
+                                                np.array(newB),
+                                                np.array(newC), e)
+        sphere_vec = e - sphere_c
+        sphere_vec /= np.linalg.norm(sphere_vec)
+        sphere_vec += e
+        simple_sphere.set_data_3d(*sphere_vec)
+        fig.canvas.draw_idle()
+
+
+    sbondA.on_changed(update)
+    sbondB.on_changed(update)
+    sbondC.on_changed(update)
+
+    resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+    button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+
+
+    def reset(event):
+        sbondA.reset()
+        sbondB.reset()
+        sbondC.reset()
+
+
+    button.on_clicked(reset)
+
+    # rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
+    # radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
+
+    plt.show()
